366ff325a8468168f48fe3e7817b3194b1c49c6d
briiiqtt <briiiqtt>
2022-04-25 15:20:41 +0900

    made temporal sample

--- socketio/app.js
+++ socketio/app.js
@@ -2,24 +2,47 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-server.listen(80)
+server.listen((port = 80), function () {
+  console.log("port:", port);
+});
 
 app.get("/", function (req, res) {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-io.on("connection", function (socket) {
-  socket.on("disconnect", function () {
-    clearInterval(socket.interval);
+io.on("connection", function (ioSocket) {
+  console.log("conn socketId:", ioSocket.id);
+
+  ioSocket.on("disconnect", function () {
+    console.log("disconn socketId:", ioSocket.id);
+    clearInterval(ioSocket.interval);
   });
 
-  socket.on("error", function (error) {
+  ioSocket.on("error", function (error) {
     console.log(error);
   });
+  /*
 
-  socket.on("msg_submit", function (data) {
-    // socket.emit("show_msg", data);
-    console.log(data);
+
+    
+
+
+*/
+  ioSocket.on("leaveRoom", (roomNum, nickname) => {
+    io.to(roomNum).emit("msgToRoom", `<System> ${nickname} has left the room`);
+    ioSocket.leave(roomNum);
   });
 
+  ioSocket.on("joinRoom", (roomNum, nickname) => {
+    ioSocket.join(roomNum);
+    io.to(roomNum).emit(
+      "msgToRoom",
+      `<System> ${nickname} has joined the room`
+    );
+  });
+
+  ioSocket.on("msgToServer", (roomNum, nickname, msg) => {
+    console.log(roomNum, nickname, msg);
+    io.to(roomNum).emit("msgToRoom", nickname + ": " + msg);
+  });
 });
